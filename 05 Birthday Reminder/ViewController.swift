1d32c5e58e05a6781d58dbdc069a581f9c75217d
macbook <yevhen.biiak@icloud.com>
2022-06-14 22:10:31 +0300

    initial commit

---
+++ 05 Birthday Reminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  05 Birthday Reminder
+//
+//  Created by Евгений Бияк on 14.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
